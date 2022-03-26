bd68a0ae01950b9e0f026fad7a7e752f0c67e2ee
Marat <Maratik79@sibmail.com>
2022-03-26 16:48:23 +0700

    Change int a

---
+++ src/Perimeter.java
@@ -0,0 +1,33 @@
+public class Perimeter {
+
+    public static void main(String[] args) {
+        int side = 2;
+        int base = 1;
+        int edge = 4;
+        System.out.println("Периметр треугольника " + getPerimeterTriangle(8, 5));
+        System.out.println("Периметр квадрата " + getPerimeterSquare(4));
+        System.out.println("Периметр окружности " + getPerimeterCircle(5));
+    }
+
+
+    public static int getPerimeterTriangle(int a, int b) {
+        int perimeterTriangle = (2 * a) + b;
+
+        return perimeterTriangle;
+    }
+
+    public static int getPerimeterSquare(int a) {
+        int perimeterSquare = 4 * a;
+
+        return perimeterSquare;
+    }
+
+    public static double getPerimeterCircle(int radius) {
+        double pi = 3.14d;
+        double perimeterCircle = 2 * pi * radius;
+
+        return perimeterCircle;
+    }
+
+
+}
